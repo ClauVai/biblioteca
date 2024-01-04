922f45b2a3bd677fc71ad58c848201c8bd05b3eb
MaryEl2121 <mariaelena.marrocco@gmail.com>
2024-01-04 17:31:22 +0100

    index, dettaglio, catalogo versione base
    css non fatto bene

--- src/main/java/it/corso/controller/HomeController.java
+++ src/main/java/it/corso/controller/HomeController.java
@@ -1,11 +1,24 @@
 package it.corso.controller;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import it.corso.service.DettaglioLibroService;
+
 
 // localhost:8080/home
 @Controller
-@RequestMapping("/home")
+@RequestMapping("/index")
 public class HomeController
 {
+	
+	@GetMapping
+	public String getPage()
+	{
+		return "index";
+	}
 	
 }
