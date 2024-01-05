ea512db7e87377684b6879cbbd85cfea58b0865e
MaryEl2121 <mariaelena.marrocco@gmail.com>
2024-01-05 09:25:32 +0100

    parte di css del catalogo

--- src/main/java/it/corso/controller/HomeController.java
+++ src/main/java/it/corso/controller/HomeController.java
@@ -1,13 +1,7 @@
 package it.corso.controller;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
-import it.corso.service.DettaglioLibroService;
-
 
 // localhost:8080/home
 @Controller
