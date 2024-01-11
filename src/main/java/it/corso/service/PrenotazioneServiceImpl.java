d23a342d3c285d128283f88bb74283b91f33e20a
ClauVai <vaiana132@gmail.com>
2024-01-11 16:25:17 +0100

    piccoli fix

--- src/main/java/it/corso/service/PrenotazioneServiceImpl.java
+++ src/main/java/it/corso/service/PrenotazioneServiceImpl.java
@@ -6,6 +6,7 @@ import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import it.corso.dao.PrenotazioneDao;
+import it.corso.model.DettaglioLibro;
 import it.corso.model.Prenotazione;
 
 @Service
@@ -21,8 +22,10 @@ public class PrenotazioneServiceImpl implements PrenotazioneService {
 	public void registraPrenotazione(int libroId, int utenteId) throws Exception {
 		Prenotazione prenotazione = new Prenotazione();
 		prenotazione.setUtente(utenteService.getUtenteById(utenteId));
-		prenotazione.setDettaglioLibro(dettaglioLibroService.getDettaglioLibro(libroId));
+		DettaglioLibro libro = dettaglioLibroService.getDettaglioLibro(libroId);
+		prenotazione.setDettaglioLibro(libro);
 		prenotazione.setDataPrenotazione(LocalDate.now());
+		libro.setStato(true);
 		prenotazioneDao.save(prenotazione);
 	}
 
@@ -41,5 +44,8 @@ public class PrenotazioneServiceImpl implements PrenotazioneService {
 	public void aggiornaRestituzioneById(int prenotazioneId) {
 		LocalDate data = LocalDate.now();
 		prenotazioneDao.updateDataRestituzioneById(prenotazioneId, data);
+		Prenotazione prenotazione = getPrenotazioneById(prenotazioneId);
+		DettaglioLibro libro = prenotazione.getDettaglioLibro();
+		libro.setStato(false);
 	}
 }
