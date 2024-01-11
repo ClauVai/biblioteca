97244391923dbe9b3113d3be6ce5bfd80014c31c
MaryEl2121 <mariaelena.marrocco@gmail.com>
2024-01-11 18:33:14 +0100

    tentativi carosello

--- src/main/resources/static/js/index.js
+++ src/main/resources/static/js/index.js
@@ -59,3 +59,25 @@
          	        return colors[randomIndex];
          	    }
          		});
+         		
+         		
+         		
+         		
+         		
+         		
+         		
+         		
+
+    document.addEventListener('DOMContentLoaded', function () {
+        // Array di colori disponibili
+        var colors = ["#EF7E6B", "#9AD0C3", "#edaf71", "#F58247", "#98D4E1"];
+
+        // Seleziona tutti gli elementi con la classe "card-title"
+        var titleElements = document.querySelectorAll('.card-title');
+
+        // Cicla attraverso gli elementi e imposta un colore casuale
+        titleElements.forEach(function (element) {
+            var randomColor = colors[Math.floor(Math.random() * colors.length)];
+            element.style.color = randomColor;
+        });
+    });
