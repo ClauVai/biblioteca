e43ec075aa3a3cc5431d2810f9fef5bf1f8b2553
ClauVai <vaiana132@gmail.com>
2024-01-14 23:34:28 +0100

    fix mQuery carosello

--- src/main/java/it/corso/dao/DettaglioLibroDao.java
+++ src/main/java/it/corso/dao/DettaglioLibroDao.java
@@ -1,13 +1,10 @@
 package it.corso.dao;
 import java.util.List;
-import org.springframework.data.jpa.repository.Modifying;
+//import org.springframework.data.jpa.repository.Modifying;
+//import org.springframework.transaction.annotation.Transactional;
 import org.springframework.data.jpa.repository.Query;
-//import org.springframework.data.jpa.repository.Query;//
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
-//import org.springframework.data.repository.query.Param;
-import org.springframework.transaction.annotation.Transactional;
-
 import it.corso.model.DettaglioLibro;
 
 public interface DettaglioLibroDao extends CrudRepository<DettaglioLibro, Integer>
